172dc0193a6eb0ba27748e44596246e0f9cb8d31
rweichitsai <rweichitsai@gmail.com>
2025-07-27 17:18:43 +0800

    Add files via upload

---
+++ components/icons/SparklesIcon.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+export const SparklesIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.293 2.293a1 1 0 010 1.414L10 17l-4 4 4-4 6.293-6.293a1 1 0 011.414 0L21 11"
+    />
+  </svg>
+);
